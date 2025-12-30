aacc1b76978c24016f7e021d7ce8de202b4a91c7
Luis Mejia <luismejiacenteno@gmail.com>
2025-12-29 22:39:45 -0600

    Refactor ticket models for improved clarity and structure

--- app/models/ticket.py
+++ app/models/ticket.py
@@ -1,22 +1,24 @@
 from enum import Enum
 from typing import Optional
-from sqlmodel import SQLModel, Field, Column, DateTime, Relationship
-from .user import User, UserPublic
 from datetime import datetime
-from app.utils import get_utc_now
+
+from sqlmodel import SQLModel, Field, Column, DateTime, Relationship
 from sqlalchemy import func
 
-
-class Status(Enum):
-    OPEN = 'open'
-    IN_PROGRESS = 'in_progress'
-    CLOSED = 'closed'
+from .user import User, UserPublic
+from app.utils import get_utc_now
 
 
-class Priority(Enum):
-    LOW = 'low'
-    MEDIUM = 'medium'
-    HIGH = 'high'
+class Status(str, Enum):
+    OPEN = "open"
+    IN_PROGRESS = "in_progress"
+    CLOSED = "closed"
+
+
+class Priority(str, Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
 
 
 class BaseTicket(SQLModel):
@@ -27,49 +29,45 @@ class BaseTicket(SQLModel):
 
 
 class Ticket(BaseTicket, table=True):
-    id: int | None = Field(primary_key=True, default=None)
-    assigned_to: int | None = Field(default=None, foreign_key='user.id')
-    created_by: int | None = Field(default=None, foreign_key='user.id')
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    assigned_to: Optional[int] = Field(default=None, foreign_key="user.id")
+    created_by: Optional[int] = Field(default=None, foreign_key="user.id")
 
     created_at: Optional[datetime] = Field(
         sa_column=Column(DateTime(timezone=True), server_default=func.now()),
         default_factory=get_utc_now,
     )
 
-    assigned_to_user: Optional[User] = Relationship(
-        sa_relationship_kwargs={
-            "foreign_keys": "[Ticket.assigned_to]"
-        }
+    assigned_to_user: Optional["User"] = Relationship(
+        sa_relationship_kwargs={"foreign_keys": "[Ticket.assigned_to]"}
     )
-
-    created_by_user: Optional[User] = Relationship(
-        sa_relationship_kwargs={
-            "foreign_keys": "[Ticket.created_by]"
-        }
+    created_by_user: Optional["User"] = Relationship(
+        sa_relationship_kwargs={"foreign_keys": "[Ticket.created_by]"}
     )
 
 
-class TicketPublic(BaseTicket, table=False):
+class TicketCreate(BaseTicket):
+    assigned_to: Optional[int] = None
+
+
+class TicketPublic(BaseTicket):
     id: int
     created_at: datetime
-    assigned_to: int
-    created_by: int
+    assigned_to: Optional[int]
+    created_by: Optional[int]
 
-    assigned_to_detail: UserPublic | None
-    created_by_detail: UserPublic
-
-
-class TicketCreate(Ticket, table=False):
-    """I need validate all fields"""
-    pass
+    assigned_to_detail: Optional[UserPublic] = None
+    created_by_detail: Optional[UserPublic] = None
 
 
 class TicketHistory(SQLModel, table=True):
-    id: int | None = Field(primary_key=True, default=None)
-    ticket_id: int | None = Field(default=None, foreign_key='ticket.id')
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    ticket_id: Optional[int] = Field(default=None, foreign_key="ticket.id")
     field_changed: str = Field(nullable=False)
-    old_value: str | None = Field(nullable=True, default=None)
-    change_by: int | None = Field(default=None, foreign_key='user.id')
+    old_value: Optional[str] = Field(default=None, nullable=True)
+    change_by: Optional[int] = Field(default=None, foreign_key="user.id")
 
     changed_at: Optional[datetime] = Field(
         sa_column=Column(DateTime(timezone=True), server_default=func.now()),
